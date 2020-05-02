317bfc27d330701aba86f3e3a8e23cee74082d91
pratham-n <64653578+pratham-n@users.noreply.github.com>
2020-05-02 21:36:58 +0530

    Created sizeof int, float, double and char
    Prints the size of int, float, char and double data types onto the screen

---
+++ sizeof_int_char_float_double.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	char a;
+	cout<<"Size of char: "<<sizeof(a)<<" byte\n";
+	int b;
+	cout<<"Size of int: "<<sizeof(b)<<" bytes\n";
+	float c;
+	cout<<"Size of float: "<<sizeof(c)<<" bytes\n";
+	double d;
+	cout<<"Size of double: "<<sizeof(d)<<" bytes\n";
+	return 0;
+}	
