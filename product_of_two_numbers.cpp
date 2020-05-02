f52c649346f211a91bb581077746eff43454b060
pratham-n <64653578+pratham-n@users.noreply.github.com>
2020-05-02 21:42:35 +0530

    Created product_of_two_numbers.cpp
    It prints the product of two numbers taken from the user.

---
+++ product_of_two_numbers.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	float a, b;
+	cout<<"Enter two numbers: ";
+	cin>>a;
+	cin>>b;
+	float product=a*b;
+	cout<<"Product = "<<product;
+	return 0;
+}	
